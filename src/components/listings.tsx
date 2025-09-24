3ad62f2285299bce14eb77753671837609c87bbb
mustafa <zbook@hp.com>
2025-09-24 14:51:03 +0300

    fix: made all home components

---
+++ src/components/listings.tsx
@@ -0,0 +1,26 @@
+import jobs from "../jobs.json";
+import Job from "../components/job";
+
+const listings = () => {
+    const recentJobs = jobs.slice(0,3);
+  return (
+
+    <section className="bg-blue-50 px-4 py-10">
+    <div className="container-xl lg:container m-auto">
+      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <!-- Job Listing 1 --> */}
+        {/* Each child in a list should have a unique "key" prop */}
+        {recentJobs.map(  (job) => (
+            <Job key={job.id} job={job} />
+        ))}
+        
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default listings
